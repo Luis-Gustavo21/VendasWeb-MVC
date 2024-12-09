ae2f79a7f8afb998f6478c57ed50a191d757071b
Luis <luis.gustavo.167@outlook.com>
2024-12-09 15:35:42 -0300

    Outras entidades e segunda migration

--- Data/VendasWeb_MVCContext.cs
+++ Data/VendasWeb_MVCContext.cs
@@ -14,6 +14,8 @@ namespace VendasWeb_MVC.Data
         {
         }
 
-        public DbSet<VendasWeb_MVC.Models.Department> Department { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
     }
 }
--- Models/Department.cs
+++ Models/Department.cs
@@ -1,8 +1,29 @@
-﻿namespace VendasWeb_MVC.Models
+﻿using System.Collections.Generic;
+using System;
+using System.Linq;
+
+namespace VendasWeb_MVC.Models
 {
     public class Department
     {
         public int Id { get; set; }
         public string Nome { get; set; }
+        public ICollection<Seller> Sellers {  get; set; } = new List<Seller>();
+        public Department()
+        {
+        }
+        public Department(int id, string nome)
+        {
+            Id = id;
+            Nome = nome;
+        }
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));
+        }
     }
 }
