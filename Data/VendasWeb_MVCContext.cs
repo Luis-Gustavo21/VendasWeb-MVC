f8accf1f42b08d1424d29d55a60dc9449219be95
Luis <luis.gustavo.167@outlook.com>
2024-12-11 00:01:29 -0300

    Seeding Service

--- Data/VendasWeb_MVCContext.cs
+++ Data/VendasWeb_MVCContext.cs
@@ -9,7 +9,7 @@ namespace VendasWeb_MVC.Data
 {
     public class VendasWeb_MVCContext : DbContext
     {
-        public VendasWeb_MVCContext (DbContextOptions<VendasWeb_MVCContext> options)
+        public VendasWeb_MVCContext(DbContextOptions<VendasWeb_MVCContext> options)
             : base(options)
         {
         }
--- Startup.cs
+++ Startup.cs
@@ -39,14 +39,17 @@ namespace VendasWeb_MVC
             services.AddDbContext<VendasWeb_MVCContext>(options =>
                     options.UseMySql(Configuration.GetConnectionString("VendasWeb_MVCContext"), builder =>
 builder.MigrationsAssembly("VendasWeb_MVC")));
+
+            services.AddScoped<SeedingService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                seedingService.Seed();
             }
             else
             {
