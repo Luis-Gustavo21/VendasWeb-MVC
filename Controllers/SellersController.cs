ad370dae4e1160a99922e7431e180f6c92392d42
Luis <luis.gustavo.167@outlook.com>
2024-12-11 01:45:16 -0300

    SellerFormViewModel e componente select para departamento

--- Controllers/SellersController.cs
+++ Controllers/SellersController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using VendasWeb_MVC.Models;
+using VendasWeb_MVC.Models.ViewModels;
 using VendasWeb_MVC.Services;
 
 namespace VendasWeb_MVC.Controllers
@@ -7,9 +8,11 @@ namespace VendasWeb_MVC.Controllers
     public class SellersController : Controller
     {
         private readonly SellerService _sellerService;
-        public SellersController(SellerService sellerService)
+        private readonly DepartmentService _departmentService;
+        public SellersController(SellerService sellerService, DepartmentService departmentService)
         {
             _sellerService = sellerService;
+            _departmentService = departmentService;
         }
         public IActionResult Index()
         {
@@ -18,7 +21,9 @@ namespace VendasWeb_MVC.Controllers
         }
         public IActionResult Create()
         {
-            return View();
+            var departments = _departmentService.FindAll();
+            var viewModel = new SellerFormViewModel { Departments = departments };
+            return View(viewModel);
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
---
+++ Services/DepartmentService.cs
@@ -0,0 +1,20 @@
+﻿using VendasWeb_MVC.Data;
+using System.Collections.Generic;
+using System.Linq;
+using VendasWeb_MVC.Models;
+
+namespace VendasWeb_MVC.Services
+{
+    public class DepartmentService
+    {
+        private readonly VendasWeb_MVCContext _context;
+        public DepartmentService(VendasWeb_MVCContext context)
+        {
+            _context = context;
+        }
+        public List<Department> FindAll()
+        {
+            return _context.Department.OrderBy(x => x.Nome).ToList();
+        }
+    }
+}
