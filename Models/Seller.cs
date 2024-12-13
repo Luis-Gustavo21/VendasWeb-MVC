c578874a4846f530c8223175946d8209056d0912
Luis <luis.gustavo.167@outlook.com>
2024-12-13 02:19:49 -0300

    Validacao

--- Models/Seller.cs
+++ Models/Seller.cs
@@ -9,21 +9,31 @@ namespace VendasWeb_MVC.Models
     public class Seller
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Nome obrigatorio")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "O tamanho do nome deve ser entre 3 e 60")]
         public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Email obrigatorio")]
+        [EmailAddress(ErrorMessage = "Insira um email valido")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Data de nascimento obrigatorio")]
         [Display(Name = "Data de nascimento")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime DataNascimento { get; set; }
+
+        [Required(ErrorMessage = "Salario base obrigatorio")]
+        [Range(100, 50000, ErrorMessage = "O salario base deve ser entre 100 e 50000")]
         [Display(Name = "Salario base")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
         public double SalarioBase { get; set; }
         public Department Department { get; set; }
+
         [Display(Name = "Departmento")]
-
         public int DepartmentId { get; set; }
-
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
         public Seller()
         {
--- Startup.cs
+++ Startup.cs
@@ -52,7 +52,6 @@ builder.MigrationsAssembly("VendasWeb_MVC")));
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
-            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
